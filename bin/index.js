64a991b01aeb95d92d734597f3f0d0daec69048b
stanislavzadiraev <zadiraeff@yandex.ru>
2023-10-13 23:59:31 +0300

    обновлено

--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,26 @@
 #!/usr/bin/env node
 
-process.title = 'node'
-process.removeAllListeners('warning')
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
 
-const I = ' - '
-const C = '../'
-
-const curpath = new Array(1).fill(C).join('')
-const parpath = new Array(3).fill(C).join('')
-
-const mdlname = ['src', 'dst'][1]
-
-Promise
-.all([
-  import(curpath + 'package.json', { assert: { type: 'json' } }),
-  import(parpath + 'package.json', { assert: { type: 'json' } }),
-])
-.then(([
-  {default: {name: curname}},
-  {default: {name: parname}},
-]) => (
-  process.title += I + parname + I + curname,
-  Promise
-  .all([
-    import(curpath + mdlname + '/index.js'),
-    import(parpath + curname + '.config.js'),
-  ])
-))
-.then(([
-  {default: mdl},
-  {default: cfg},
-]) => 
-  process.argv.slice(2).reduce((acc, cur) => acc && acc[cur], mdl)(cfg)
-)
+readFile(new URL(join('..', 'package.json'), import.meta.url))
+	.then(data => JSON.parse(data))
+	.then(({ name, main }) =>
+		!name && Promise.reject(new Error('name lost')) ||
+		!main && Promise.reject(new Error('main lost')) ||
+		Promise.resolve({ name, main })
+	)
+	.then(({ name, main }) =>
+		Promise.all([
+			import(join('..', main))
+				.catch(() => ({ default: undefined })),
+			import(join('..', '..', '..', `${name}.config.js`))
+				.catch(() => ({ default: undefined })),
+		])
+	)
+	.then(([
+		{ default: proc },
+		{ default: conf },
+	]) =>
+		process.argv.slice(2).reduce((acc, cur) => acc[cur], proc)(conf)
+	)
